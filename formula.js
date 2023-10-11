fbc7d78047f0d8d9f28889dedaec2138cd078210
DerylFeyza <muhammadderylfeyza@gmail.com>
2023-10-11 20:51:13 +0700

    add: shapes and its formula

--- formula.js
+++ formula.js
@@ -1,7 +1,40 @@
-countpersegi = () =>{
-  let panjang = document.getElementById('panjang').value;
-  let lebar = document.getElementById('lebar').value;
-  let hasil = panjang * lebar;
+countpersegi = () => {
+let panjang = parseFloat(document.getElementById('panjang').value);
+let lebar = parseFloat(document.getElementById('lebar').value);
+let hasil = panjang * lebar;
 
-  document.getElementById('hasil').value = hasil;
+document.getElementById('hasil').value = hasil;
+}
+
+countsegitiga = () => {
+let alas = parseFloat(document.getElementById('alas').value);
+let tinggi = parseFloat(document.getElementById('tinggi').value);
+let hasil = alas * tinggi / 2;
+
+document.getElementById('hasil').value = hasil;
+}
+
+countlingkaran = () => {
+let radius = parseFloat(document.getElementById('radius').value);
+let hasil = Math.PI * (radius * radius);
+
+document.getElementById('hasil').value = hasil;
+}
+
+countbelahketupat = () => {
+let d1 = parseFloat(document.getElementById('d1').value);
+let d2 = parseFloat(document.getElementById('d2').value);
+let hasil = d1 * d2 / 2;
+
+document.getElementById('hasil').value = hasil;
+}
+
+counttrapesium = () => {
+let s1 = parseFloat(document.getElementById('s1').value);
+let s2 = parseFloat(document.getElementById('s2').value);
+let tinggi = parseFloat(document.getElementById('tinggi').value);
+
+let hasil = (s1 + s2) * tinggi / 2;
+
+document.getElementById('hasil').value = hasil;
 }
